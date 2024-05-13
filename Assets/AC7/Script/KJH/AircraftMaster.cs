238778171f8afb8a1b2babce8d834e229d670aeb
권지호 <imperiaiiist@naver.com>
2024-05-13 15:55:08 +0900

    (Update) 미사일 안전신관 기능 추가

--- Assets/AC7/Script/KJH/AircraftMaster.cs
+++ Assets/AC7/Script/KJH/AircraftMaster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//참조용 클래스. 하위 컴포넌트에 접근할 때 사용. 웬만하면 여기서 하위 컴포넌트를 수정하지 말 것
 public class AircraftMaster : MonoBehaviour
 {
     [SerializeField] bool aiControl;
