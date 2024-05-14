23454c7e680f4b286db07f6132eb1930b252b93f
변석진 <bsj2234@naver.com>
2024-05-14 13:15:54 +0900

    [Update] 자막시스템 업데이트 자막을 string으로 반환하는대신 직접 자막의 띄워주도록 변경

--- Assets/AC7/Script/BSJ/SubtitleManager.cs
+++ Assets/AC7/Script/BSJ/SubtitleManager.cs
@@ -12,6 +12,18 @@ public class SubtitleManager : SceneSingleton<SubtitleManager>
 {
     [SerializeField] private TextAsset _subtitleCsvAsset;
     private Dictionary<string, string> _subtitleDictionary = new Dictionary<string, string>();
+    [SerializeField] private TMPro.TMP_Text _text;
+
+    //대사 지속시간 계산법 _baseDuration + text.length * addtionalTimeRatio
+    //예                        2        +  10 * .15f  10글자면 3.5초
+    private float _baseDuration = 1f;//기본 지속시간
+    private float _addtionalDurationRatio = .05f;
+    private float _duration;
+
+    //테스트용 코드
+    [SerializeField] private bool _test;
+    [SerializeField] private string _testKey;
+
     private void Awake()
     {
         Init();
@@ -34,25 +46,76 @@ public class SubtitleManager : SceneSingleton<SubtitleManager>
         }
     }
 
+    private void Update()
+    {
+        if(_duration > 0f)
+        {
+            _duration -= Time.deltaTime;
+        }
+        else
+        {
+            _text.enabled = false;
+        }
+
+        if(_test == true)
+        {
+            ShowSubtitle(_testKey);
+            _test = false;
+        }
+    }
+
     /// <summary>
-    /// 키를 받아 자막을 반환하는 메서드
+    /// 키를 받아 자막을 출력하는 메서드
     /// 
-    /// Hit1,경보! 우측 엔진에 피격당했다!
-    ///Hit2,"피격 상태 심각, 즉시 지원 요청한다!"
-    ///MissileLaunch,폭스 2
-    ///Kill1,목표 적중! 다음 목표로 이동!
-    ///Kill2,"적군 격파 확인, 작전 성공적!"
-    ///Kill3,정확한 타격! 목표 파괴 완료!
-    ///StartMission1,"임무 시작, 모두 집중하고 조심하자!"
-    ///StartMission2,"전투 준비 완료, 행운을 빕니다!"
+    /*
+        ID,English,Original
+        Hit1,"Alert! The right engine has been hit!","경보! 우측 엔진에 피격당했다!"
+        Hit2,"The damage is severe, request immediate support!","피격 상태 심각, 즉시 지원 요청한다!"
+        MissileLaunch,"Fox 2","폭스 2"
+        Kill1,"Target hit! Moving to the next target!","목표 적중! 다음 목표로 이동!"
+        Kill2,"Enemy down confirmed, operation successful!","적군 격파 확인, 작전 성공적!"
+        Kill3,"Accurate strike! Target destroyed!","정확한 타격! 목표 파괴 완료!"
+        StartMission1,"Mission start, everyone stay focused and be careful!","임무 시작, 모두 집중하고 조심하자!"
+        StartMission2,"Combat ready, good luck!","전투 준비 완료, 행운을 빕니다!"
+     */
     /// 
     /// </summary>
     /// <param name="key"></param>
-    /// <returns></returns>
-    public string GetSubtitle(string key)
+    public void ShowSubtitle(string key)
     {
-        return _subtitleDictionary[key];
-    }
+        Debug.Assert(_subtitleDictionary.ContainsKey(key), "Wrong subtitle key");
+        _text.text = _subtitleDictionary[key];
 
+        _duration = _baseDuration + _text.text.Length * _addtionalDurationRatio;
+
+        _text.enabled = true;
+    }
+    /// <summary>
+    /// 키를 받아 지정된 시간동안 자막을 출력하는 메서드
+    /// 
+    /*
+        ID,English,Original
+        Hit1,"Alert! The right engine has been hit!","경보! 우측 엔진에 피격당했다!"
+        Hit2,"The damage is severe, request immediate support!","피격 상태 심각, 즉시 지원 요청한다!"
+        MissileLaunch,"Fox 2","폭스 2"
+        Kill1,"Target hit! Moving to the next target!","목표 적중! 다음 목표로 이동!"
+        Kill2,"Enemy down confirmed, operation successful!","적군 격파 확인, 작전 성공적!"
+        Kill3,"Accurate strike! Target destroyed!","정확한 타격! 목표 파괴 완료!"
+        StartMission1,"Mission start, everyone stay focused and be careful!","임무 시작, 모두 집중하고 조심하자!"
+        StartMission2,"Combat ready, good luck!","전투 준비 완료, 행운을 빕니다!"
+     */
+    /// 
+    /// </summary>
+    /// <param name="key">키</param>
+    /// <param name="time">지속시간</param>
+    public void ShowSubtitleWithTime(string key, float time)
+    {
+        Debug.Assert(_subtitleDictionary.ContainsKey(key), "Wrong subtitle key");
+        _text.text = _subtitleDictionary[key];
+
+        _duration = time;
+
+        _text.enabled = true;
+    }
 
 }
