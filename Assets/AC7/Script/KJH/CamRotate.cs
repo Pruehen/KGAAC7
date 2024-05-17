2d0c1310f9bd26691363d41ac59c204d3b960502
권지호 <imperiaiiist@naver.com>
2024-05-17 17:37:14 +0900

    [Fix] 카메라가 추적하는 타겟이 없어졌을 때 화면이 순간적으로 돌아가는 현상 수정

--- Assets/AC7/Script/KJH/CamRotate.cs
+++ Assets/AC7/Script/KJH/CamRotate.cs
@@ -52,8 +52,11 @@ public class CamRotate : MonoBehaviour
             if(vehicleCombat != null)
             {
                 viewTargetTrf = vehicleCombat.transform;//레이더 타겟으로 카메라의 타겟을 지정            
+            }            
+            else
+            {
+                viewTargetTrf = null;
             }
-            
 
             if(viewTargetTrfTemp != viewTargetTrf)//타겟이 변경되었을 경우
             {
