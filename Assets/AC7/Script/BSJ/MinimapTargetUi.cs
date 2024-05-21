da53713be28aa5ff905b93881c6c555c5f6f1c80
변석진 <bsj2234@naver.com>
2024-05-21 21:02:06 +0900

    [Update] 미니맵 각도 제대로 나오게 수정

--- Assets/AC7/Script/BSJ/MinimapTargetUi.cs
+++ Assets/AC7/Script/BSJ/MinimapTargetUi.cs
@@ -13,10 +13,11 @@ public class MinimapTargetUi : MonoBehaviour
     private VehicleCombat _vehicleCombat;
     private Transform _targetTransform;
     private Transform _virtualMinimapPlayerAxis;
-
+    Vector3 _gap;
+    Vector3 _offset;
     public void Init(Transform transform, Transform virtualMinimapPlayerAxis 
         , float minx, float maxx, float miny, float maxy
-        , float iconSize, float ratio)
+        , float iconSize, float ratio, Vector3 offset)
     {
         _vehicleCombat = transform.GetComponent<VehicleCombat>();
         _targetTransform = _vehicleCombat?.transform ?? transform;
@@ -40,6 +41,8 @@ public class MinimapTargetUi : MonoBehaviour
         {
             transform.GetComponent<Guided>().OnRemove += RemoveTargetUi;
         }
+        _gap = new Vector3((_maxx - _minx)/2f, (_maxy - _miny)/2f, 0f);
+        _offset = offset;
     }
 
     private void Update()
@@ -50,11 +53,43 @@ public class MinimapTargetUi : MonoBehaviour
         }
 
         //위치 로컬로 변환
+        //Vector3 resultPos = _virtualMinimapPlayerAxis.InverseTransformPoint(_targetTransform.position);
+        //resultPos = new Vector3(resultPos.x, resultPos.z, 0f);
+        //resultPos *= _ratio;
+        //resultPos = new Vector3(Mathf.Clamp(resultPos.x, _minx, _maxx), Mathf.Clamp(resultPos.y, _miny, _maxy), 0f);
+        //transform.localPosition = resultPos;
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        //Transform playerTrf = kjh.GameManager.Instance.player.transform;
+        //Vector3 playerPos = playerTrf.position;
+        //Vector3 playerToTarget = -playerPos + _targetTransform.position;
+
+        //Vector3 result = Quaternion.Inverse(playerTrf.rotation) * playerToTarget;
+        //result.y = 0f;
+        //result = result.normalized;
+        //float dist = playerToTarget.magnitude;
+
+        //result *= dist;
         Vector3 resultPos = _virtualMinimapPlayerAxis.InverseTransformPoint(_targetTransform.position);
         resultPos = new Vector3(resultPos.x, resultPos.z, 0f);
         resultPos *= _ratio;
-        resultPos = new Vector3(Mathf.Clamp(resultPos.x, _minx, _maxx), Mathf.Clamp(resultPos.y, _miny, _maxy), 0f);
-        transform.localPosition = resultPos;
+
+        Vector3 dir = resultPos.normalized;
+        bool xin = resultPos.y < _miny || resultPos.y > _maxy;
+        _gap.y = (resultPos.y > 0f) ? _maxy : _miny;
+        if (Mathf.Abs(resultPos.x) > _gap.x || Mathf.Abs(resultPos.y) > _gap.y) 
+        {
+            Vector3 dist3D = new Vector3(Mathf.Abs(_gap.x / dir.x), Mathf.Abs(_gap.y / dir.y), 0f);
+            float dist = Mathf.Min(dist3D.x, dist3D.y);
+            transform.localPosition = dir * dist;
+        }
+        else
+        {
+
+            transform.localPosition = dir * resultPos.magnitude;
+        }
+
+
+
         //방향 로컬로 변환
         Quaternion _currentItemLookRot = Quaternion.LookRotation(-_targetTransform.forward);
         Quaternion resultRot = _currentItemLookRot * Quaternion.Inverse(_virtualMinimapPlayerAxis.rotation);
