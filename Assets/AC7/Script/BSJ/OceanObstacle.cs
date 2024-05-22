d101b18f6097e61c5612ea8071f37e84b17fff68
변석진 <bsj2234@naver.com>
2024-05-22 17:31:03 +0900

    [Fix] 오타수정

--- Assets/AC7/Script/BSJ/OceanObstacle.cs
+++ Assets/AC7/Script/BSJ/OceanObstacle.cs
@@ -16,7 +16,7 @@ public class OceanObstacle : MonoBehaviour
         {
             if (target.IsDead() == true)
                 return;
-            GameObject pooledItem = ObjectPoolManager.Instance.DequeueObject(_spashVfxPrefab);
+            GameObject pooledItem = ObjectPoolManager.Instance.DequeueObject(_splashVfxPrefab);
             pooledItem.transform.position = collision.transform.position;
             ObjectPoolManager.Instance.EnqueueObject(pooledItem, 10f);            
             target.TakeDamage(9999999f);
