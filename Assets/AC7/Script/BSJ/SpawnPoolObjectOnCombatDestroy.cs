1a246efff5bb56bfcfadf18b011e6f8d4fcc43fa
변석진 <bsj2234@naver.com>
2024-05-25 15:39:38 +0900

    [Update] 시체 오프셋 수정 및 마스터 비히클에서 destroyed 이벤트 호출시 spawnpoolobject 사용해서 스폰하게 만듬

---
+++ Assets/AC7/Script/BSJ/SpawnPoolObjectOnCombatDestroy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoolObjectOnCombatDestroy : MonoBehaviour
+{
+    [SerializeField] GameObject _spawnObjectPrifab;
+    [SerializeField] float _scale = 1f;
+    [SerializeField] bool _onWater = false;
+    [SerializeField] float _lifeTime = 300f;
+    VehicleMaster _masterCombat;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _masterCombat = GetComponent<VehicleMaster>();
+        _masterCombat.OnCombatDestroy.AddListener(Spawn);
+    }
+
+
+    public void Spawn()
+    {
+        GameObject item = ObjectPoolManager.Instance.DequeueObject(_spawnObjectPrifab);
+        item.transform.localScale = Vector3.one * _scale;
+        if(_onWater)
+        {
+            Vector3 onWaterPos = transform.position;
+            onWaterPos.y = 0f;
+            item.transform.position = onWaterPos;
+        }
+        item.transform.position = transform.position;
+        ObjectPoolManager.Instance.EnqueueObject(item, _lifeTime);
+    }
+}
